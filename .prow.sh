4e0a3cab07c12f696650f71552b3470f9d76b699
Patrick Ohly <patrick.ohly@intel.com>
2019-02-26 21:19:12 +0100

    .prow.sh: initial Prow job setup
    There's nothing to configure at the moment, we can use the share
    prow.sh without additional configuration.

---
+++ .prow.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# TODO: move into job
+CSI_PROW_GINKO_PARALLEL="-p -nodes 40" # default was 7
+
+# Simulates canary test job.
+# TODO: canary periodic job
+#CSI_PROW_BUILD_JOB=false
+#CSI_PROW_KUBERNETES_VERSION=latest
+#CSI_PROW_HOSTPATH_CANARY=canary
+
+CSI_PROW_TESTS_SANITY="sanity"
+
+. release-tools/prow.sh
+
+main
