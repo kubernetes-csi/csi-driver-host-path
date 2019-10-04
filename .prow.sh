11c2c4f803285ba6d3d3d26ff15d9b7435669070
Michelle Au <msau@google.com>
2019-10-03 19:11:35 -0700

    DO NOT MERGE: testing 1.16

--- .prow.sh
+++ .prow.sh
@@ -11,6 +11,9 @@ CSI_PROW_GINKO_PARALLEL="-p -nodes 40" # default was 7
 CSI_PROW_HOSTPATH_DRIVER_NAME="hostpath.csi.k8s.io"
 
 CSI_PROW_TESTS_SANITY="sanity"
+CSI_PROW_KUBERNETES_VERSION="1.16.2"
+CSI_PROW_KUBERNETES_DEPLOYMENT=1.16
+CSI_PROW_TESTS="parallel parallel-alpha"
 
 . release-tools/prow.sh
 
