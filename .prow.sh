fd9acbc3c24805e2fc118410809a2ac28ebd0de8
Michelle Au <msau@google.com>
2019-10-23 16:02:42 -0700

    DO NOT MERGE: testing kind fix

--- .prow.sh
+++ .prow.sh
@@ -11,6 +11,10 @@ CSI_PROW_GINKO_PARALLEL="-p -nodes 40" # default was 7
 CSI_PROW_HOSTPATH_DRIVER_NAME="hostpath.csi.k8s.io"
 
 CSI_PROW_TESTS_SANITY="sanity"
+CSI_PROW_KIND_VERSION="86bc23d84ac12dcb56a0528890736e2c347c2dc3"
+CSI_PROW_KUBERNETES_VERSION="release-1.15"
+CSI_PROW_DEPLOYMENT="kubernetes-1.15"
+CSI_PROW_TESTS="parallel"
 
 . release-tools/prow.sh
 
