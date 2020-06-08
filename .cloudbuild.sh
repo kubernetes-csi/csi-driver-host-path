5126fbe92ce325b308890ad9a56194002476aa4d
Patrick Ohly <patrick.ohly@intel.com>
2020-06-08 10:54:30 +0200

    Revert "cloud build: initialize support for running commands in Dockerfile"
    This reverts commit 19e36ae7a171674d3a35104c3eb021c42c3f1129.
    
    These changes are now also in csi-release-tools/prow.sh.

--- .cloudbuild.sh
+++ .cloudbuild.sh
@@ -1,11 +1,1 @@
-#! /bin/bash
-
-# shellcheck disable=SC1091
-. release-tools/prow.sh
-
-if find . -name Dockerfile | grep -v ^./vendor | xargs --no-run-if-empty cat | grep -q ^RUN; then
-    # Needed for "RUN apk" on non linux/amd64 platforms.
-    (set -x; docker run --rm --privileged multiarch/qemu-user-static --reset -p yes)
-fi
-
-gcr_cloud_build
+release-tools/cloudbuild.sh
