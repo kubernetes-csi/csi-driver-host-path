b1ab9441df8859b53e5765deb0feeed5b67c1120
Niels de Vos <ndevos@ibm.com>
2026-07-10 16:24:02 +0200

    Remove csi-test version from local config
    The latest release-tools include the csi-test v5.5.0 version by default
    now.

--- .prow.sh
+++ .prow.sh
@@ -25,8 +25,6 @@ CSI_PROW_GINKO_PARALLEL="-p -nodes 40" # default was 7
 CSI_PROW_HOSTPATH_DRIVER_NAME="hostpath.csi.k8s.io"
 
 CSI_PROW_TESTS_SANITY="sanity"
-# TODO(nixpanic): remove when csi-release-tools PR#314 is merged
-CSI_PROW_SANITY_VERSION="v5.5.0"
 
 # We need to hardcode e2e version for resizer for now, because
 # we need fixes from latest release-1.31 branch for all e2es to pass.
